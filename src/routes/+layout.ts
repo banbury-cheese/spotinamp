4ca50908e72ce7ec10ed29e0a5011206aaa24327
Kay <kumartanmay315@gmail.com>
2024-04-17 22:02:45 +0100

    initial layout

---
+++ src/routes/+layout.ts
@@ -0,0 +1,8 @@
+import type { LayoutLoad } from "./$types";
+import type { Entry } from "$lib/types";
+
+export const load: LayoutLoad = async ({ fetch }) => {
+  const response = await fetch("api/entries");
+  const entries: Entry[] = await response.json();
+  return { entries };
+};
