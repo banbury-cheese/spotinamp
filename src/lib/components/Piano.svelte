d82d74feb7cae5b65a920ae6cb6125d8b3255a57
Kay <kumartanmay315@gmail.com>
2024-04-22 15:00:26 +0100

    Adding tone.js to piano keys

--- src/lib/components/Piano.svelte
+++ src/lib/components/Piano.svelte
@@ -1,76 +1,313 @@
 <script lang="ts">
   import type { Entry } from "$lib/types";
-  import { page } from '$app/stores';
+  import { page } from "$app/stores";
+  import * as Tone from "tone";
+  import { onMount } from "svelte";
   export let entries: Entry[];
 
+  import C1 from "$lib/audio/C1.wav";
+  import C2 from "$lib/audio/C2.wav";
+  import C3 from "$lib/audio/C3.wav";
+  import C4 from "$lib/audio/C4.wav";
+  import C5 from "$lib/audio/C5.wav";
+  import C6 from "$lib/audio/C6.wav";
+
+  import G1 from "$lib/audio/G1.wav";
+  import G2 from "$lib/audio/G2.wav";
+  import G3 from "$lib/audio/G3.wav";
+  import G4 from "$lib/audio/G4.wav";
+  import G5 from "$lib/audio/G5.wav";
+
+  import B1 from "$lib/audio/B1.wav";
+  import B2 from "$lib/audio/B2.wav";
+  import B3 from "$lib/audio/B3.wav";
+  import B4 from "$lib/audio/B4.wav";
+  import B5 from "$lib/audio/B5.wav";
 
   let notesInOrder = [
-		'B7', 'A7', 'G7', 'F7', 'E7', 'D7', 'C7', 
-		'B6', 'A6', 'G6', 'F6', 'E6', 'D6', 'C6', 
-		'B5', 'A5', 'G5', 'F5', 'E5', 'D5', 'C5', 
-		'B4', 'A4', 'G4', 'F4', 'E4', 'D4', 'C4', 
-		'B3', 'A3', 'G3', 'F3', 'E3', 'D3', 'C3', 
-		'B2', 'A2', 'G2', 'F2', 'E2', 'D2', 'C2', 
-		'B1', 'A1', 'G1', 'F1', 'E1', 'D1', 'C1',
-		'B0', 'A0'
-	];
+    "B7",
+    "A7",
+    "G7",
+    "F7",
+    "E7",
+    "D7",
+    "C7",
+    "B6",
+    "A6",
+    "G6",
+    "F6",
+    "E6",
+    "D6",
+    "C6",
+    "B5",
+    "A5",
+    "G5",
+    "F5",
+    "E5",
+    "D5",
+    "C5",
+    "B4",
+    "A4",
+    "G4",
+    "F4",
+    "E4",
+    "D4",
+    "C4",
+    "B3",
+    "A3",
+    "G3",
+    "F3",
+    "E3",
+    "D3",
+    "C3",
+    "B2",
+    "A2",
+    "G2",
+    "F2",
+    "E2",
+    "D2",
+    "C2",
+    "B1",
+    "A1",
+    "G1",
+    "F1",
+    "E1",
+    "D1",
+    "C1",
+    "B0",
+    "A0",
+  ];
 
-	let blackNotesInOrder = [
-		'Bb7', 'Ab7', 'Gb7', '', 'Eb7', 'Db7', '',
-		'Bb6', 'Ab6', 'Gb6', '', 'Eb6', 'Db6', '',
-		'Bb5', 'Ab5', 'Gb5', '', 'Eb5', 'Db5', '',
-		'Bb4', 'Ab4', 'Gb4', '', 'Eb4', 'Db4', '',
-		'Bb3', 'Ab3', 'Gb3', '', 'Eb3', 'Db3', '',
-		'Bb2', 'Ab2', 'Gb2', '', 'Eb2', 'Db2', '',
-		'Bb1', 'Ab1', 'Gb1', '', 'Eb1', 'Db1', '',
-		'Bb0', 'Ab0'
-	];
+  let blackNotesInOrder = [
+    "Bb7",
+    "Ab7",
+    "Gb7",
+    "",
+    "Eb7",
+    "Db7",
+    "",
+    "Bb6",
+    "Ab6",
+    "Gb6",
+    "",
+    "Eb6",
+    "Db6",
+    "",
+    "Bb5",
+    "Ab5",
+    "Gb5",
+    "",
+    "Eb5",
+    "Db5",
+    "",
+    "Bb4",
+    "Ab4",
+    "Gb4",
+    "",
+    "Eb4",
+    "Db4",
+    "",
+    "Bb3",
+    "Ab3",
+    "Gb3",
+    "",
+    "Eb3",
+    "Db3",
+    "",
+    "Bb2",
+    "Ab2",
+    "Gb2",
+    "",
+    "Eb2",
+    "Db2",
+    "",
+    "Bb1",
+    "Ab1",
+    "Gb1",
+    "",
+    "Eb1",
+    "Db1",
+    "",
+    "Bb0",
+    "Ab0",
+  ];
 
-	let notesInOrderReverse = [
-		'C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1',
-		'C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2',
-		'C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3',
-		'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4',
-		'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5',
-		'C6', 'D6', 'E6', 'F6', 'G6', 'A6', 'B6',
-		'C7', 'D7', 'E7', 'F7', 'G7', 'A7', 'B7',
-		'C8', 'D7'
+  let notesInOrderReverse = [
+    "C1",
+    "D1",
+    "E1",
+    "F1",
+    "G1",
+    "A1",
+    "B1",
+    "C2",
+    "D2",
+    "E2",
+    "F2",
+    "G2",
+    "A2",
+    "B2",
+    "C3",
+    "D3",
+    "E3",
+    "F3",
+    "G3",
+    "A3",
+    "B3",
+    "C4",
+    "D4",
+    "E4",
+    "F4",
+    "G4",
+    "A4",
+    "B4",
+    "C5",
+    "D5",
+    "E5",
+    "F5",
+    "G5",
+    "A5",
+    "B5",
+    "C6",
+    "D6",
+    "E6",
+    "F6",
+    "G6",
+    "A6",
+    "B6",
+    "C7",
+    "D7",
+    "E7",
+    "F7",
+    "G7",
+    "A7",
+    "B7",
+    "C8",
+    "D7",
+  ];
 
-	];
+  let blackNotesInOrderReverse = [
+    "Db1",
+    "Eb1",
+    "",
+    "Gb1",
+    "Ab1",
+    "Bb1",
+    "",
+    "Db2",
+    "Eb2",
+    "",
+    "Gb2",
+    "Ab2",
+    "Bb2",
+    "",
+    "Db3",
+    "Eb3",
+    "",
+    "Gb3",
+    "Ab3",
+    "Bb3",
+    "",
+    "Db4",
+    "Eb4",
+    "",
+    "Gb4",
+    "Ab4",
+    "Bb4",
+    "",
+    "Db5",
+    "Eb5",
+    "",
+    "Gb5",
+    "Ab5",
+    "Bb5",
+    "",
+    "Db6",
+    "Eb6",
+    "",
+    "Gb6",
+    "Ab6",
+    "Bb6",
+    "",
+    "Db7",
+    "Eb7",
+    "",
+    "Gb7",
+    "Ab7",
+    "Bb7",
+    "",
+    "Db1",
+    "",
+  ];
 
-	let blackNotesInOrderReverse = [
-		'Db1', 'Eb1', '', 'Gb1', 'Ab1', 'Bb1', '',
-		'Db2', 'Eb2', '', 'Gb2', 'Ab2', 'Bb2','',
-		'Db3', 'Eb3', '', 'Gb3', 'Ab3', 'Bb3','', 
-		'Db4', 'Eb4', '', 'Gb4', 'Ab4', 'Bb4','',
-		'Db5', 'Eb5', '', 'Gb5', 'Ab5', 'Bb5','',
-		'Db6', 'Eb6', '', 'Gb6', 'Ab6', 'Bb6','', 
-  	'Db7', 'Eb7', '', 'Gb7', 'Ab7', 'Bb7',	'', 
-	  'Db1',	'',
+  let synth: any;
+  const playWhiteKey = (idx: number) => {
+    synth.triggerAttackRelease(notesInOrder[idx % notesInOrder.length], "2n");
+  };
+  const playBlackKey = (idx: number) => {
+    synth.triggerAttackRelease(
+      blackNotesInOrder[idx % blackNotesInOrder.length],
+      "2n"
+    );
+  };
 
-	];
+  onMount(() => {
+    const vol = new Tone.Volume(-8).toDestination();
+    synth = new Tone.Sampler({
+      urls: {
+        C1,
+        C2,
+        C3,
+        C4,
+        C5,
+        C6,
+        G1,
+        G2,
+        G3,
+        G4,
+        G5,
+        B1,
+        B2,
+        B3,
+        B4,
+        B5,
+      },
+    }).connect(vol);
+  });
 </script>
 
 {#each entries as entry, idx}
   <div class="key-container" id="entry-{idx}">
-    <a href="/{entry.slug}">
+    <a
+      href="/{entry.slug}"
+      on:click={() => {
+        playWhiteKey(idx);
+      }}
+    >
       <div class="entryName">
         {entry.name}
       </div>
     </a>
-    <div class=" 
+    <div
+      class=" 
 			{entry.slug === $page.params.name ? 'text-black' : 'text-grey4'}
-			{
-      // horizontal ? 'top-1 left-0 w-20 h-8' :
-       'right-0 top-0 w-8 h-20'} absolute text-xs tracking-wide  flex justify-center items-center rounded-[10rem]">
-				<!-- {#if horizontal} -->
-				<!-- {notesInOrderReverse[idx % notesInOrder.length]} -->
-
-				<!-- {:else} -->
-				{notesInOrder[idx % notesInOrder.length]}
-				<!-- {/if} -->
-			</div>
-    <div class="black-key">
+      'right-0 top-0 w-8 h-20'} absolute text-xs tracking-wide flex justify-center items-center rounded-[10rem]"
+    >
+      {notesInOrder[idx % notesInOrder.length]}
+    </div>
+    <div
+      class="black-key"
+      on:click={() => {
+        playBlackKey(idx);
+      }}
+      on:keydown={(event) => {
+        if (event.key === "Enter") {
+          playBlackKey(idx);
+        }
+      }}
+      role="button"
+      tabindex="0"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
